fefa438704d9857417bfd0826d273fa86ea3d89e
Dayne Dewar <daynedewar@yahoo.com>
2021-02-23 22:24:26 -0500

    commit tree 05c97eb76740ab84f820acaf79461c955ec4877f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+/***** Step 1 *****/
+
+const main = document.querySelector('main#main');
+main.remove();
+
+/***** Step 2 *****/
+
+let newHeader = document.createElement('h1');
+
+/***** Step 3 *****/
+
+newHeader.setAttribute('id', 'victory');
+
+/***** Step 4 *****/
+
+newHeader.innerHTML = "Dayne is the champion";
+document.body.appendChild(newHeader);
